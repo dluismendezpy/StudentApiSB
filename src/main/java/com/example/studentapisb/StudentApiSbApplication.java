61e1df20be7daf7d552f24110d226aefe4c2fea7
Luis Mendez <dluismendezdj@gmail.com>
2023-01-05 19:41:41 -0400

    feat: create project

---
+++ src/main/java/com/example/studentapisb/StudentApiSbApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentapisb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentApiSbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentApiSbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studentapisb/StudentApiSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentapisb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentApiSbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
